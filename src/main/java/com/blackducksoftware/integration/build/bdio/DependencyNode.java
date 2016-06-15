f1465f618a42764b332be7e8b229d15cf0e126f2
Eric Kerwin <ekerwin@blackducksoftware.com>
2016-06-15 19:32:38 -0400

    adding classpath builder and re-using the passed in list

--- src/main/java/com/blackducksoftware/integration/build/bdio/DependencyNode.java
+++ src/main/java/com/blackducksoftware/integration/build/bdio/DependencyNode.java
@@ -1,17 +1,14 @@
 package com.blackducksoftware.integration.build.bdio;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class DependencyNode {
 	private final Gav gav;
-	private final List<DependencyNode> children = new ArrayList<>();
+	private final List<DependencyNode> children;
 
 	public DependencyNode(final Gav gav, final List<DependencyNode> children) {
 		this.gav = gav;
-		if (null != children && children.size() > 0) {
-			this.children.addAll(children);
-		}
+		this.children = children;
 	}
 
 	public Gav getGav() {
