97f8d11b16fa16f95d80ea10d17d94e716f35312
Paulo Santos <psantos@blackducksoftware.com>
2016-07-22 12:16:54 -0400

    update to the constants used to generate BDIO file output
    Updated the file extension to jsonld because the file contains ld+json
    data.  Also added a constant for the media type to uplaod the file to
    the hub to define the media type in one place for all potential plugins.

--- src/main/java/com/blackducksoftware/integration/build/bdio/Constants.java
+++ src/main/java/com/blackducksoftware/integration/build/bdio/Constants.java
@@ -1,6 +1,7 @@
 package com.blackducksoftware.integration.build.bdio;
 
 public class Constants {
-	public static final String BDIO_FILE_SUFFIX = "_bdio.json";
+	public static final String BDIO_FILE_SUFFIX = "_bdio.jsonld";
+	public static final String BDIO_FILE_MEDIA_TYPE = "application/ld+json";
 
 }
