f88efe737521cd0b9eb39ae4ac7ce2687f3fdc2f
Eric Kerwin <ekerwin@blackducksoftware.com>
2016-10-12 19:16:34 -0400

    creating release

--- src/test/java/com/blackducksoftware/integration/build/utils/FilePathGavExtractorTest.java
+++ src/test/java/com/blackducksoftware/integration/build/utils/FilePathGavExtractorTest.java
@@ -1,11 +1,14 @@
 package com.blackducksoftware.integration.build.utils;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 
 public class FilePathGavExtractorTest {
 	@Test
 	public void testExtractingMavenGav() {
 		final FilePathGavExtractor extractor = new FilePathGavExtractor();
+		assertNotNull(extractor);
 	}
 
 }
