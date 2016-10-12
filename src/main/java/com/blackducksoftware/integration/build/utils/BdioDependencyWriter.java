436648cbe45651cc9987bc39b9ca615d9549a59c
Eric Kerwin <ekerwin@blackducksoftware.com>
2016-10-12 19:04:25 -0400

    adding common bdio writer

---
+++ src/main/java/com/blackducksoftware/integration/build/utils/BdioDependencyWriter.java
@@ -0,0 +1,33 @@
+package com.blackducksoftware.integration.build.utils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.blackducksoftware.integration.build.DependencyNode;
+import com.blackducksoftware.integration.build.bdio.BdioConverter;
+import com.blackducksoftware.integration.build.bdio.CommonBomFormatter;
+
+public class BdioDependencyWriter {
+	Logger logger = LoggerFactory.getLogger(BdioDependencyWriter.class);
+
+	public void write(final File outputDirectory, final String filename, final String hubProjectName,
+			final DependencyNode rootNode) throws IOException {
+		// if the directory doesn't exist yet, let's create it
+		outputDirectory.mkdirs();
+
+		final File file = new File(outputDirectory, filename);
+		logger.info(String.format("Generating file: %s", file.getCanonicalPath()));
+
+		try (final OutputStream outputStream = new FileOutputStream(file)) {
+			final BdioConverter bdioConverter = new BdioConverter();
+			final CommonBomFormatter commonBomFormatter = new CommonBomFormatter(bdioConverter);
+			commonBomFormatter.writeProject(outputStream, hubProjectName, rootNode);
+		}
+
+	}
+}
